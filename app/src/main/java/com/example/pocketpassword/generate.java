f57f6a40e91eec4182e333bf059879bb1529fea1
DMartinCodes <dennis.martin.1010@outlook.com>
2023-01-22 01:39:00 -0500

    Revamp of generate class to create passwords in a more cryptographically secure manner.

--- app/src/main/java/com/example/pocketpassword/generate.java
+++ app/src/main/java/com/example/pocketpassword/generate.java
@@ -1,47 +1,30 @@
 package com.example.pocketpassword;
-import java.util.Random;
+import java.security.SecureRandom;
 import java.lang.Math;
 
 
 public class generate {
 
-    static String lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
-    String upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String digits = "0123456789";
-    String specChars = "!@#$%^&*()_+~";
+     char[] secChar = {'0','1','2','3','4','5','6','7','8','9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#',
+            '$', '%', '&', '(', ')', '*', '+', ',', '-', '.',
+            '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^',
+            '_', '`', '{', '|', '}', '~'};
 
     public String genPass(){
 
-        int lowerMax = lowerAlpha.length();
-        int upperMax = upperAlpha.length();
-        int digitMax = digits.length();
-        int specMax = specChars.length();
-        int min = 1;
-
         String passWord = "";
+        SecureRandom randomChar = new SecureRandom();
 
-        while(passWord.length() < 8){
-            if(passWord.length() % 2 == 0) {
-                int x = (int) Math.floor(Math.random() * (lowerMax - min) + min);
-                passWord += lowerAlpha.charAt(x);
-            } else {
-                int z = (int)Math.floor(Math.random() * (digitMax - min) + min);
-                passWord += digits.charAt(z);
-            }
+        while(passWord.length() < 15) {
+            int ranIndex = randomChar.nextInt(secChar.length);
+            char ranChar = secChar[ranIndex];
+            passWord += ranChar;
         }
-
-        for(int i = 0; i < 7; i++){
-            if(i % 2 == 0) {
-                int x = (int) Math.floor(Math.random() * (upperMax - min) + min);
-                passWord += upperAlpha.charAt(x);
-            } else {
-                int z = (int)Math.floor(Math.random() * (specMax - min) + min);
-                passWord += specChars.charAt(z);
-            }
-        }
-
-
-
 
         return passWord;
     }
